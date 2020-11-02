ebf8d95e0946be07b85196a72adbbe9bb674f9bf
Sun <gabehttps@gmail.com>
2020-11-02 11:29:28 -0800

    Particle Argument Type

---
+++ src/main/java/services/headpat/spigotextensions/brigadier/ArgumentTypes/ParticleArgumentType.java
@@ -0,0 +1,37 @@
+package services.headpat.spigotextensions.brigadier.ArgumentTypes;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import org.bukkit.Particle;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
+public class ParticleArgumentType implements ArgumentType<Particle> {
+	@Override
+	public Particle parse(StringReader reader) throws CommandSyntaxException {
+		Particle particle;
+		try {
+			particle = Particle.valueOf(reader.readString());
+		} catch (Exception e) {
+			throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherParseException().createWithContext(reader, null);
+		}
+		return particle;
+	}
+
+	@Override
+	public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+		return null;
+	}
+
+	@Override
+	public Collection<String> getExamples() {
+		return Arrays.stream(Particle.values()).map(Enum::name).collect(Collectors.toList());
+	}
+}
