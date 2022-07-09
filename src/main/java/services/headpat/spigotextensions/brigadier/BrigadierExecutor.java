9db6bf39c86a2e1ca844f19971b91746c68855d3
gabehxd <gabehttps@gmail.com>
2022-07-08 20:45:12 -0700

    remove Commodore

--- src/main/java/services/headpat/spigotextensions/brigadier/BrigadierExecutor.java
+++ src/main/java/services/headpat/spigotextensions/brigadier/BrigadierExecutor.java
@@ -3,42 +3,38 @@ package services.headpat.spigotextensions.brigadier;
 import com.google.common.collect.ObjectArrays;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.mojang.brigadier.suggestion.Suggestion;
+import com.mojang.brigadier.suggestion.Suggestions;
 import lombok.NonNull;
-import me.lucko.commodore.Commodore;
-import me.lucko.commodore.CommodoreProvider;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.PluginCommand;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.TabExecutor;
 
-import java.util.function.Function;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 /**
- * Creates autocomplete suggestions (using commodore) and onCommand handling automatically.
+ * Creates autocomplete suggestions and onCommand handling automatically.
  */
-public class BrigadierExecutor implements CommandExecutor {
+public class BrigadierExecutor implements TabExecutor {
     protected final CommandDispatcher<CommandSender> commandDispatcher;
-    private final LiteralCommandNode<CommandSender> commandNode;
 
     /**
      * Returning 1 from an executor in the LiteralArgumentBuilder will act as true and 0 and below will act as false.
      *
-     * @param dispatcherConsumer The consumer to register commands with and to return the LiteralCommandNode.
+     * @param dispatcherConsumer The consumer to register commands with.
      */
-    public BrigadierExecutor(@NonNull Function<CommandDispatcher<CommandSender>, LiteralCommandNode<CommandSender>> dispatcherConsumer) {
+    public BrigadierExecutor(@NonNull Consumer<CommandDispatcher<CommandSender>> dispatcherConsumer) {
         this.commandDispatcher = new CommandDispatcher<>();
-        this.commandNode = dispatcherConsumer.apply(commandDispatcher);
+        dispatcherConsumer.accept(commandDispatcher);
     }
 
-    public void registerCommand(JavaPlugin plugin, PluginCommand command) {
+    public void registerCommand(PluginCommand command) {
         command.setExecutor(this);
-        if (CommodoreProvider.isSupported()) {
-            Commodore commodore = CommodoreProvider.getCommodore(plugin);
-            commodore.register(command, commandNode);
-        }
+        command.setTabCompleter(this);
     }
 
     @Override
@@ -46,15 +42,30 @@ public class BrigadierExecutor implements CommandExecutor {
         try {
             int result = this.commandDispatcher.execute(getCommandString(alias, args), sender);
             if (result <= 0) {
-                return false;
+                sendUsageMessage(sender, this.commandDispatcher);
+                return true;
             }
         } catch (CommandSyntaxException e) {
             if (e.getMessage() != null)
                 sender.sendMessage(ChatColor.RED + e.getMessage());
 
-            return false;
+            sendUsageMessage(sender, this.commandDispatcher);
+            return true;
         }
         return true;
+    }
+
+    @Override
+    public final @NonNull List<String> onTabComplete(@NonNull CommandSender sender, @NonNull Command command, @NonNull String alias, @NonNull String[] args) {
+        String commandString = getCommandString(alias, args);
+        Suggestions suggestions = this.commandDispatcher.getCompletionSuggestions(this.commandDispatcher.parse(commandString, sender)).join();
+        return suggestions.getList().stream().map(Suggestion::getText).collect(Collectors.toList());
+    }
+
+    private static void sendUsageMessage(@NonNull CommandSender sender, @NonNull CommandDispatcher<CommandSender> commandDispatcher) {
+        sender.sendMessage(ChatColor.RED + "Usages:");
+        for (String s : commandDispatcher.getAllUsage(commandDispatcher.getRoot(), sender, true))
+            sender.sendMessage(ChatColor.RED + "/" + s);
     }
 
     public final String getCommandString(String alias, String[] args) {
